07d22b8c9be4fd8bae4f78ee43618e132e4d3bfe
Prima <widianiprima@gmail.com>
2024-11-12 08:39:39 +0700

    feat(graphql): add TypeDefs for required entities in themes project

--- server/index.js
+++ server/index.js
@@ -1,17 +1,61 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-const typeDefs = `
+
+
+const typeDefs = `#graphql
+    type User {
+        _id: ID!
+        name: String!
+        username: String!
+        email: String!
+        password: String!
+    }
+
+    type Post {
+        _id: ID!
+        content: String!
+        tags: [String]
+        imgUrl: String
+        authorId: ID!
+        comments: [Comment]
+        likes: [Like]
+        createdAt: String
+        updatedAt: String
+    }
+
+    type Comment {
+        content: String!
+        username: String!
+        createdAt: String
+        updatedAt: String
+    }
+
+    type Like {
+        username: String!
+        createdAt: String
+        updatedAt: String
+    }
+
+    type Follow {
+        followingId: ID
+        followerId: ID
+        createdAt: String
+        updatedAt: String
+    }
+
     type Query {
-        hello: String
+        getUser(id: ID!): User
+        getPosts: [Post]
+    }
+
+    type Mutation {
+        createUser(name: String!, username: String!, email: String!, password: String!): User
+        createPost(content: String!, tags: [String], imgUrl: String, authorId: ID!): Post
     }
 `;
 
-const resolvers = {
-	Query: {
-		hello: () => "Hello, world!",
-	},
-};
+
 
 const server = new ApolloServer({
 	typeDefs,
